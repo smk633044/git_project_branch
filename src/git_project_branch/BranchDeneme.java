e26eaa46ebef0fbd07b31298951db425f79f745e
Baki <smk633044@gmail.com>
2020-08-22 16:18:26 +0200

    featureden commit

--- src/git_project_branch/BranchDeneme.java
+++ src/git_project_branch/BranchDeneme.java
@@ -5,7 +5,7 @@ public class BranchDeneme {
 	public static void main(String[] args) {
 
 	System.out.println("16:28 - master branchten ilk commit");	
-		
+	System.out.println("18:36 - feature branchten ilk satir");	
 		
 		
 		
