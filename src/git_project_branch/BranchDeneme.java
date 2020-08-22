ff888b3c5a9f452d7d38d01cae9d6ce5e556ec0a
Baki <smk633044@gmail.com>
2020-08-22 16:10:39 +0200

    ilk commit from master

--- src/git_project_branch/BranchDeneme.java
+++ src/git_project_branch/BranchDeneme.java
@@ -3,8 +3,12 @@ package git_project_branch;
 public class BranchDeneme {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	System.out.println("16:28 - master branchten ilk commit");	
+		
+		
+		
+		
 	}
 
 }
